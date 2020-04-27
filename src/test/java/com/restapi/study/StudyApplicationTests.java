ebed9f66ac9abc2a0317a8eb4eb7a1cd06d81272
LeeRyangHwa <iryanghwa@gmail.com>
2020-04-27 19:51:02 +0900

    restapi_project

---
+++ src/test/java/com/restapi/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
